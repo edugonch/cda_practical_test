e9d1ada36ec7751ccd267ab18c5f8bf2cfc3ec71
Eduardo González <eduardo.gonzales@admios-sa.com>
2019-02-04 09:15:52 -0600

    Config wdgi app

--- wsgi.py
+++ wsgi.py
@@ -5,4 +5,4 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rfp_project.settings")
 # This application object is used by the development server
 # as well as any WSGI server configured to use this file.
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+application = get_wsgi_application() 
